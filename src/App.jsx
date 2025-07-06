29c420a10e65d226a330e4312a406e7e46c051d7
Yousuf Islam <yousufislam7900@gmail.com>
2025-07-06 21:27:26 +0600

    data load done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
-
+import Blogs from './components/Blogs/Blogs'
+import Header from './components/Header/Header'
 function App() {
 
   return (
     <>
       <div>
-        <h1 className='font-bold'>Knowledge Cafe</h1>
+        <Header></Header>
+        <Blogs></Blogs>
       </div>
     </>
   )
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='font-bold text-4xl'>Knowledge Cafe</h1>
+        </div>
+    );
+};
+export default Header;
