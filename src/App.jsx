5cca159581ae9a949ef643ecf53514cbae98943d
Yousuf Islam <yousufislam7900@gmail.com>
2025-07-06 21:00:45 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='font-bold'>Knowledge Cafe</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
